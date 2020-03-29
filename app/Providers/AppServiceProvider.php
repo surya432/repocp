fcd683db0dd6a416fc89bfa8fc33ed455bd7b728
surya432 <hadisurya295@gmail.com>
2020-03-29 21:33:39 +0700

    modif appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
