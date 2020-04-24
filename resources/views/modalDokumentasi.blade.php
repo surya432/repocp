b3947bf8a43ba3965ac032f57599e1368b00d5f6
surya432 <hadisurya295@gmail.com>
2020-04-25 02:00:15 +0700

    modif data

--- resources/views/modalDokumentasi.blade.php
+++ resources/views/modalDokumentasi.blade.php
@@ -5,10 +5,9 @@
             <p class="font-produk-deskripsi">{{$b['deskripsi']}}</p>
 
         </div>
-        @if(is_null($b['imagesMedia'])) <div class="row wrap-small-img">
+        @if(count($b['imagesMedia'])< 1) <div class="row wrap-small-img">
             <div class="column img-demo">
-                <img class="demo cursor" src="{{url('/images/'.$b['images'])}}" onclick="currentSlide(1)"
-                    alt="{{$b['title']}}">
+                <img class="demo cursor" src="{{url('/images/'.$b['images'])}}" onclick="currentSlide(1)" alt="{{$b['title']}}">
             </div>
         </div>
         <div class="mySlides ">
@@ -16,22 +15,20 @@
         </div>
         @else <div class="row wrap-small-img">
             @for($i=0;$i< count($b['imagesMedia']);$i++) <div class="column img-demo">
-                <img class="demo cursor" src="{{url('/images/'.$b['imagesMedia'][$i]['path']) }}"
-                    onclick="currentSlide({{$i+1}})" alt="{{$b['title']}}">
-            </div>
-            @endfor
-    </div>
-
-    @for($i=0;$i< count($b['imagesMedia']);$i++) <div class="mySlides ">
-        <img src="{{url('/images/'.$b['imagesMedia'][$i]['path']) }}" alt="{{$b['title']}}" />
+                <img class="demo cursor" src="{{url('/images/'.$b['imagesMedia'][$i]['path']) }}" onclick="currentSlide({{$i+1}})" alt="{{$b['title']}}">
         </div>
         @endfor
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    </div>
+
+            @for($i=0;$i< count($b['imagesMedia']);$i++) <div class="mySlides ">
+            <img src="{{url('/images/'.$b['imagesMedia'][$i]['path']) }}" alt="{{$b['title']}}" />
+            </div>
+            @endfor
+            <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+            <a class="next" onclick="plusSlides(1)">&#10095;</a>
         @endif
 
         <div class="caption-container">
-
             <p class="font-produk-deskripsi">{{$b['content']}}</p>
             <ul class="list-inline m-b-40">
                 <li>Date : {{$b['tanggal']}}</li>
